c80db8cf45fd7529b7d1ebea8f56db4720da4eb8
Mercy-Muchai <mercy.muchai@andela.com>
2018-04-06 15:56:55 +0300

    function to make http request socket.io function to set event and callback

--- app.js
+++ app.js
@@ -16,6 +16,22 @@ const server = http.createServer(app);
 // initialize a new instance of socketIo
 const io = socketIo(server);
 
-const getApiAndEmit = "TODO"
+io.on("connection", socket => {
+  console.log("New client connected"), setInterval(
+    () => getApiAndEmit(socket),
+    10000
+  );
+  socket.on("disconnect", () => console.log("Client disconnected"));
+});
+
+const getApiAndEmit = async socket => {
+  try {
+    const res = await axios.get(
+      "https://api.darksky.net/forecast/d2f17b3f91b2787739f38f1f0dc018bb/37.8267,-122.4233"
+    );
+  } catch (error) {
+    console.error(`Error: ${error.code}`);
+  }
+};
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
